6c8356832ad565af8bfbc6f16a2c682fc9d8d56e
东灯 <43312495+Lampese@users.noreply.github.com>
2023-01-18 00:24:32 +0800

    commonJs -> ESM

--- test/replace.js
+++ test/replace.js
@@ -4,23 +4,23 @@ import * as fs from "fs";
 let dirs = fs.readdirSync("../build/behavior_packs/gen/scripts/src");
 
 dirs.forEach((f) => {
-  try {
-    let content = fs.readFileSync("../build/behavior_packs/gen/scripts/src/" + f);
-    let c = content
-      .toString()
-      .split("\n")
-      .filter((val) => !val.startsWith("import") && !val.endsWith('"@minecraft/server";'));
-    c.unshift(`class BlockLocation {
+    try {
+        let content = fs.readFileSync("../build/behavior_packs/gen/scripts/src/" + f);
+        let c = content
+            .toString()
+            .split("\n")
+            .filter((val) => !val.startsWith("import") && !val.endsWith('"@minecraft/server";'));
+        c.unshift(`class BlockLocation {
         constructor(x, y, z) {
           this.x = x;
           this.y = y;
           this.z = z;
         }
       }`);
-    c = c.join("\n");
-    fs.writeFileSync(f, c);
-    console.log("writring: ", f, { flag: "a+" });
-  } catch (e) {
-    console.log(e);
-  }
+        c = c.join("\n");
+        fs.writeFileSync(f, c);
+        console.log("writring: ", f, { flag: "a+" });
+    } catch (e) {
+        console.log(e);
+    }
 });
