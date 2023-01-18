5d9d031b8fd7c7ee4b13a2e9fb1d319e1ed5a549
东灯 <43312495+Lampese@users.noreply.github.com>
2023-01-18 20:41:20 +0800

    Improve the effect of DLA

--- scripts/src/DLA.ts
+++ scripts/src/DLA.ts
@@ -9,16 +9,11 @@ class Point {
     this.y = y;
   }
   private Vary(steplength: number) {
-    function rand() {
-      let p = Math.random();
-      if (p > 0.5) return Math.random();
-      else return -Math.random();
-    }
     return [this.x + rand() * steplength, this.y + rand() * steplength];
   }
   Walk(width: number, steplength: number) {
     let [tox, toy] = this.Vary(steplength);
-    while (tox < 0 || tox > width || toy < 0 || toy > width) {
+    while (Math.abs(tox) > width / 2 || Math.abs(toy) > width / 2) {
       [tox, toy] = this.Vary(steplength);
     }
     [this.x, this.y] = [tox, toy];
@@ -29,16 +24,28 @@ class DLASystem {
   width: number;
   maxWalk: number;
   iterations: number;
+  step: number;
+  T: number;
   Walkering: Point[];
   Stucked: Point[];
 
-  constructor(width: number, maxWalk: number, iterations: number) {
+  constructor(
+    width: number,
+    maxWalk: number,
+    iterations: number,
+    step: number,
+    T: number,
+    stuck: BlockLocation[] = []
+  ) {
     this.width = width;
     this.maxWalk = maxWalk;
     this.iterations = iterations;
+    this.T = T;
     this.Walkering = [];
     this.Stucked = [];
-    this.Stucked.push(new Point(this.width / 2, this.width / 2));
+    this.step = step;
+    if (stuck.length === 0) this.Stucked.push(new Point(0, 0));
+    else this.Stucked = stuck.map((v) => new Point(v.x, v.z));
     while (this.Walkering.length < maxWalk) {
       this.Walkering.push(randPoint(this.width));
     }
@@ -51,7 +58,7 @@ class DLASystem {
           if (this.Walkering[j].Stucked === true) continue;
           this.Walkering[j].Walk(this.width, 1);
           for (let k = 0; k < this.Stucked.length; ++k) {
-            if (checkStuck(this.Walkering[j], this.Stucked[k])) {
+            if (checkStuck(this.Walkering[j], this.Stucked[k], this.step)) {
               this.Walkering[j].Stucked = true;
               this.Stucked.push(this.Walkering[j]);
               break;
@@ -60,25 +67,42 @@ class DLASystem {
         }
         this.Walkering = this.Walkering.filter((v) => v.Stucked === false);
       }
+      while (this.Walkering.length < this.maxWalk && this.T > 1) {
+        this.Walkering.push(randPoint(this.width));
+        this.T *= 0.995;
+      }
     }
-    return this.Stucked.map((v) => new BlockLocation(v.x, 0, v.y));
+    return this.Stucked.map((v) => new BlockLocation(Math.round(v.x), 0, Math.round(v.y)));
   }
 }
 
+function rand(): number {
+  let p = Math.random();
+  if (p > 0.5) return Math.random();
+  else return -Math.random();
+}
+
 function randPoint(width: number): Point {
-  return new Point(Math.random() * width, Math.random() * width);
+  return new Point(rand() * (width / 2), rand() * (width / 2));
 }
 
 function distance(a: Point, b: Point): number {
   return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
 
-function checkStuck(a: Point, b: Point): boolean {
-  return distance(a, b) < 1;
+function checkStuck(a: Point, b: Point, step: number): boolean {
+  return distance(a, b) < 1.8 * step;
 }
 
-function DLA(width: number, maxWalk: number, iterations: number): BlockLocation[] {
-  const sys = new DLASystem(width, maxWalk, iterations);
+function DLA(
+  width: number,
+  maxWalk: number,
+  iterations: number,
+  step: number,
+  T: number,
+  stuck: BlockLocation[] = []
+): BlockLocation[] {
+  const sys = new DLASystem(width, maxWalk, iterations, step, T, stuck);
   return sys.run();
 }
 
