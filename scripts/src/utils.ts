ea62b8955158c0e2caefd3e02d67dfe7e7493332
CAIMEOX <1853884864@qq.com>
2023-01-19 12:47:11 +0800

    Update utils

--- scripts/src/utils.ts
+++ scripts/src/utils.ts
@@ -12,9 +12,25 @@ function LocationTrans(pos: Vector3): BlockLocation {
   return new BlockLocation(Math.round(pos.x), Math.round(pos.y), Math.round(pos.z));
 }
 
+function BAdd(p1: BlockLocation, p2: BlockLocation): BlockLocation {
+  return new BlockLocation(p1.x + p2.x, p1.y + p2.y, p2.z + p2.z);
+}
+
+function BMul(p: BlockLocation, k: number): BlockLocation {
+  return new BlockLocation(p.x * k, p.y * k, p.z * k);
+}
+
 function now(): string {
   let date = new Date();
   return ["[", date.toTimeString().slice(0, 8), "]"].join("");
 }
 
-export { LocationAdd, Tellraw, LocationTrans };
+function random(min: number, max: number): number {
+  return Math.random() * (max - min) + min;
+}
+
+function getDistacne(p1: BlockLocation, p2: BlockLocation): number {
+  return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y) + (p1.z - p2.z) * (p1.z - p2.z));
+}
+
+export { LocationAdd, Tellraw, LocationTrans, BAdd, BMul, random, getDistacne };
