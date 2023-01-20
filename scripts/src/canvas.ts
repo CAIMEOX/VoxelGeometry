c10a77823ad689d0b2b2c1c351cc08472c1ab0e3
东灯 <43312495+Lampese@users.noreply.github.com>
2023-01-20 14:12:19 +0800

    More standardized

--- scripts/src/canvas.ts
+++ scripts/src/canvas.ts
@@ -12,6 +12,7 @@ class Transform {
   matrix: Array<number>;
   stack: any[];
   m: number[] = [];
+
   constructor(context: Context) {
     this.context = context;
     this.matrix = [1, 0, 0, 1, 0, 0];
@@ -40,7 +41,7 @@ class Transform {
   //==========================================
 
   save() {
-    let matrix = this.cloneMatrix(this.getMatrix());
+    const matrix = this.cloneMatrix(this.getMatrix());
     this.stack.push(matrix);
 
     if (this.context) this.context.save();
@@ -48,7 +49,7 @@ class Transform {
 
   restore() {
     if (this.stack.length > 0) {
-      let matrix = this.stack.pop();
+      const matrix = this.stack.pop();
       this.setMatrix(matrix);
     }
 
@@ -80,17 +81,16 @@ class Transform {
   }
 
   rotate(rad: number) {
-    let c = Math.cos(rad);
-    let s = Math.sin(rad);
-    let m11 = this.matrix[0] * c + this.matrix[2] * s;
-    let m12 = this.matrix[1] * c + this.matrix[3] * s;
-    let m21 = this.matrix[0] * -s + this.matrix[2] * c;
-    let m22 = this.matrix[1] * -s + this.matrix[3] * c;
+    const c = Math.cos(rad);
+    const s = Math.sin(rad);
+    const m11 = this.matrix[0] * c + this.matrix[2] * s;
+    const m12 = this.matrix[1] * c + this.matrix[3] * s;
+    const m21 = this.matrix[0] * -s + this.matrix[2] * c;
+    const m22 = this.matrix[1] * -s + this.matrix[3] * c;
     this.matrix[0] = m11;
     this.matrix[1] = m12;
     this.matrix[2] = m21;
     this.matrix[3] = m22;
-
     this.setTransform();
   }
 
@@ -108,7 +108,7 @@ class Transform {
   //==========================================
 
   rotateDegrees(deg: number) {
-    let rad = (deg * Math.PI) / 180;
+    const rad = (deg * Math.PI) / 180;
     this.rotate(rad);
   }
 
@@ -132,14 +132,14 @@ class Transform {
   }
 
   multiply(matrix: { m: number[] }) {
-    let m11 = this.matrix[0] * matrix.m[0] + this.matrix[2] * matrix.m[1];
-    let m12 = this.matrix[1] * matrix.m[0] + this.matrix[3] * matrix.m[1];
+    const m11 = this.matrix[0] * matrix.m[0] + this.matrix[2] * matrix.m[1];
+    const m12 = this.matrix[1] * matrix.m[0] + this.matrix[3] * matrix.m[1];
 
-    let m21 = this.matrix[0] * matrix.m[2] + this.matrix[2] * matrix.m[3];
-    let m22 = this.matrix[1] * matrix.m[2] + this.matrix[3] * matrix.m[3];
+    const m21 = this.matrix[0] * matrix.m[2] + this.matrix[2] * matrix.m[3];
+    const m22 = this.matrix[1] * matrix.m[2] + this.matrix[3] * matrix.m[3];
 
-    let dx = this.matrix[0] * matrix.m[4] + this.matrix[2] * matrix.m[5] + this.matrix[4];
-    let dy = this.matrix[1] * matrix.m[4] + this.matrix[3] * matrix.m[5] + this.matrix[5];
+    const dx = this.matrix[0] * matrix.m[4] + this.matrix[2] * matrix.m[5] + this.matrix[4];
+    const dy = this.matrix[1] * matrix.m[4] + this.matrix[3] * matrix.m[5] + this.matrix[5];
 
     this.matrix[0] = m11;
     this.matrix[1] = m12;
@@ -151,13 +151,13 @@ class Transform {
   }
 
   invert() {
-    let d = 1 / (this.matrix[0] * this.matrix[3] - this.matrix[1] * this.matrix[2]);
-    let m0 = this.matrix[3] * d;
-    let m1 = -this.matrix[1] * d;
-    let m2 = -this.matrix[2] * d;
-    let m3 = this.matrix[0] * d;
-    let m4 = d * (this.matrix[2] * this.matrix[5] - this.matrix[3] * this.matrix[4]);
-    let m5 = d * (this.matrix[1] * this.matrix[4] - this.matrix[0] * this.matrix[5]);
+    const d = 1 / (this.matrix[0] * this.matrix[3] - this.matrix[1] * this.matrix[2]);
+    const m0 = this.matrix[3] * d;
+    const m1 = -this.matrix[1] * d;
+    const m2 = -this.matrix[2] * d;
+    const m3 = this.matrix[0] * d;
+    const m4 = d * (this.matrix[2] * this.matrix[5] - this.matrix[3] * this.matrix[4]);
+    const m5 = d * (this.matrix[1] * this.matrix[4] - this.matrix[0] * this.matrix[5]);
     this.matrix[0] = m0;
     this.matrix[1] = m1;
     this.matrix[2] = m2;
@@ -172,8 +172,8 @@ class Transform {
   //==========================================
 
   transformPoint(pt: { x: any; y: any }): Point {
-    let x = pt.x;
-    let y = pt.y;
+    const x = pt.x;
+    const y = pt.y;
     return {
       x: x * this.matrix[0] + y * this.matrix[2] + this.matrix[4],
       y: x * this.matrix[1] + y * this.matrix[3] + this.matrix[5],
@@ -183,6 +183,7 @@ class Transform {
 class Point {
   x: number;
   y: number;
+
   constructor(x: number, y: number) {
     this.x = x;
     this.y = y;
@@ -192,17 +193,21 @@ class Bitmap {
   width: number;
   height: number;
   data: BlockLocation[];
+
   constructor(w: number, h: number) {
     this.width = Math.floor(w);
     this.height = Math.floor(h);
     this.data = [];
   }
+
   dot(x: number, y: number) {
     this.data.push(new BlockLocation(x, 0, y));
   }
+
   getContext() {
     return new Context(this);
   }
+
   getBlocks() {
     return this.data;
   }
@@ -212,29 +217,36 @@ class Context {
   _lineWidth: number;
   transform: Transform;
   _clip: Line[] | null = null;
-  path: (String | Point | object)[];
+  path: (string | Point | object)[];
   pathstart: any;
+
   constructor(bitmap: Bitmap) {
     this.path = [];
     this.bitmap = bitmap;
     this._lineWidth = 1;
     this.transform = new Transform(this);
   }
+
   save() {
     this.transform.save();
   }
+
   translate(x: any, y: any) {
     this.transform.translate(x, y);
   }
+
   rotate(angle: any) {
     this.transform.rotate(angle);
   }
+
   scale(sx: any, sy: any) {
     this.transform.scale(sx, sy);
   }
+
   restore() {
     this.transform.restore();
   }
+
   fillRect(x: number, y: number, w: number, h: number) {
     for (let i = x; i < x + w; i++) {
       for (let j = y; j < y + h; j++) {
@@ -242,6 +254,7 @@ class Context {
       }
     }
   }
+
   clearRect(x: number, y: number, w: number, h: number) {
     for (let i = x; i < x + w; i++) {
       for (let j = y; j < y + h; j++) {
@@ -249,6 +262,7 @@ class Context {
       }
     }
   }
+
   strokeRect(x: number, y: number, w: number, h: number) {
     for (let i = x; i < x + w; i++) {
       this.bitmap.dot(i, y);
@@ -259,51 +273,61 @@ class Context {
       this.bitmap.dot(x + w, j);
     }
   }
+
   getImageData() {
     return this.bitmap;
   }
+
   beginPath() {
     this.path = [];
   }
+
   moveTo(x: any, y: any) {
     return this._moveTo(new Point(x, y));
   }
+
   _moveTo(pt: Point) {
     pt = this.transform.transformPoint(pt);
     this.pathstart = pt;
     this.path.push([PATH_COMMAND.MOVE, pt]);
   }
+
   lineTo(x: any, y: any) {
     return this._lineTo(new Point(x, y));
   }
+
   _lineTo(pt: Point) {
     this.path.push([PATH_COMMAND.LINE, this.transform.transformPoint(pt)]);
   }
+
   quadraticCurveTo(cp1x: any, cp1y: any, x: any, y: any) {
-    let cp1 = this.transform.transformPoint(new Point(cp1x, cp1y));
-    let pt = this.transform.transformPoint(new Point(x, y));
+    const cp1 = this.transform.transformPoint(new Point(cp1x, cp1y));
+    const pt = this.transform.transformPoint(new Point(x, y));
     this.path.push([PATH_COMMAND.QUADRATIC_CURVE, cp1, pt]);
   }
+
   bezierCurveTo(cp1x: any, cp1y: any, cp2x: any, cp2y: any, x: any, y: any) {
     this._bezierCurveTo(new Point(cp1x, cp1y), new Point(cp2x, cp2y), new Point(x, y));
   }
+
   _bezierCurveTo(cp1: Point, cp2: Point, pt: Point) {
     cp1 = this.transform.transformPoint(cp1);
     cp2 = this.transform.transformPoint(cp2);
     pt = this.transform.transformPoint(pt);
     this.path.push([PATH_COMMAND.BEZIER_CURVE, cp1, cp2, pt]);
   }
+
   arc(x: number, y: number, rad: number, start: number, end: number, clockwise: any) {
     function calcPoint(angle: number) {
-      let px = x + Math.sin(angle) * rad;
-      let py = y + Math.cos(angle) * rad;
+      const px = x + Math.sin(angle) * rad;
+      const py = y + Math.cos(angle) * rad;
       return new Point(px, py);
     }
     if (start > end) end += Math.PI * 2;
 
-    let step = Math.PI / 16;
+    const step = Math.PI / 16;
     if (clockwise) {
-      let temp = end;
+      const temp = end;
       end = start + Math.PI * 2;
       start = temp;
     }
@@ -313,29 +337,33 @@ class Context {
     }
     this._lineTo(calcPoint(end));
   }
+
   clip() {
     this._clip = pathToLines(this.path);
   }
+
   closePath() {
     this.path.push([PATH_COMMAND.LINE, this.pathstart]);
   }
+
   stroke() {
     pathToLines(this.path).forEach((line) => this.drawLine(line));
   }
+
   drawLine(line: { start: { x: number; y: number }; end: { x: number; y: number } }) {
     let x0 = Math.floor(line.start.x);
     let y0 = Math.floor(line.start.y);
-    let x1 = Math.floor(line.end.x);
-    let y1 = Math.floor(line.end.y);
-    let dx = Math.abs(x1 - x0),
+    const x1 = Math.floor(line.end.x);
+    const y1 = Math.floor(line.end.y);
+    const dx = Math.abs(x1 - x0),
       sx = x0 < x1 ? 1 : -1;
-    let dy = Math.abs(y1 - y0),
+    const dy = Math.abs(y1 - y0),
       sy = y0 < y1 ? 1 : -1;
     let err = (dx > dy ? dx : -dy) / 2;
-    while (true) {
+    for (;;) {
       this.bitmap.dot(x0, y0);
       if (x0 === x1 && y0 === y1) break;
-      let e2 = err;
+      const e2 = err;
       if (e2 > -dx) {
         err -= dy;
         x0 += sx;
@@ -346,14 +374,15 @@ class Context {
       }
     }
   }
+
   fill() {
-    let lines = pathToLines(this.path);
-    let bounds = calcMinimumBounds(lines);
+    const lines = pathToLines(this.path);
+    const bounds = calcMinimumBounds(lines);
     for (let j = bounds.y2 - 1; j >= bounds.y; j--) {
-      let ints = calcSortedIntersections(lines, j);
+      const ints = calcSortedIntersections(lines, j);
       for (let i = 0; i < ints.length; i += 2) {
-        let start = Math.floor(ints[i]);
-        let end = Math.floor(ints[i + 1]);
+        const start = Math.floor(ints[i]);
+        const end = Math.floor(ints[i + 1]);
         for (let ii = start; ii <= end; ii++) {
           if (ii == start) {
             this.bitmap.dot(ii, j);
@@ -368,10 +397,11 @@ class Context {
       }
     }
   }
+
   pixelInsideClip(x: number, y: number) {
     if (!this._clip) return true;
-    let ints = calcSortedIntersections(this._clip, y);
-    let left = ints.filter((inter) => inter < x);
+    const ints = calcSortedIntersections(this._clip, y);
+    const left = ints.filter((inter) => inter < x);
     if (left.length % 2 === 0) {
       return false;
     } else {
@@ -391,8 +421,8 @@ class Line {
   }
 }
 function calcQuadraticAtT(p: Point[], t: number) {
-  let x = (1 - t) * (1 - t) * p[0].x + 2 * (1 - t) * t * p[1].x + t * t * p[2].x;
-  let y = (1 - t) * (1 - t) * p[0].y + 2 * (1 - t) * t * p[1].y + t * t * p[2].y;
+  const x = (1 - t) * (1 - t) * p[0].x + 2 * (1 - t) * t * p[1].x + t * t * p[2].x;
+  const y = (1 - t) * (1 - t) * p[0].y + 2 * (1 - t) * t * p[1].y + t * t * p[2].y;
   return new Point(x, y);
 }
 
@@ -411,7 +441,7 @@ function calcBezierAtT(p: Point[], t: number) {
 }
 
 function calcMinimumBounds(lines: any[]) {
-  let bounds = { x: Number.MAX_VALUE, y: Number.MAX_VALUE, x2: Number.MIN_VALUE, y2: Number.MIN_VALUE };
+  const bounds = { x: Number.MAX_VALUE, y: Number.MAX_VALUE, x2: Number.MIN_VALUE, y2: Number.MIN_VALUE };
   function checkPoint(pt: { x: number; y: number }) {
     bounds.x = Math.min(bounds.x, pt.x);
     bounds.y = Math.min(bounds.y, pt.y);
@@ -426,12 +456,12 @@ function calcMinimumBounds(lines: any[]) {
 }
 
 function calcSortedIntersections(lines: string | any[], y: number) {
-  let xlist: number[] = [];
+  const xlist: number[] = [];
   for (let i = 0; i < lines.length; i++) {
-    let A = lines[i].start;
-    let B = lines[i].end;
+    const A = lines[i].start;
+    const B = lines[i].end;
     if ((A.y < y && B.y >= y) || (B.y < y && A.y >= y)) {
-      let xval: number = A.x + ((y - A.y) / (B.y - A.y)) * (B.x - A.x);
+      const xval: number = A.x + ((y - A.y) / (B.y - A.y)) * (B.x - A.x);
       xlist.push(xval);
     }
   }
@@ -441,7 +471,7 @@ function calcSortedIntersections(lines: string | any[], y: number) {
 }
 
 function pathToLines(path: any[]) {
-  let lines: Line[] = [];
+  const lines: Line[] = [];
   let curr: Point | null = null;
 
   path.forEach(function (cmd: any[]) {
@@ -449,22 +479,22 @@ function pathToLines(path: any[]) {
       curr = cmd[1];
     }
     if (cmd[0] == PATH_COMMAND.LINE) {
-      let pt = cmd[1];
+      const pt = cmd[1];
       lines.push(new Line(curr!, pt));
       curr = pt;
     }
     if (cmd[0] == PATH_COMMAND.QUADRATIC_CURVE) {
-      let pts: Point[] = [curr, cmd[1], cmd[2]];
+      const pts: Point[] = [curr, cmd[1], cmd[2]];
       for (let t = 0; t < 1; t += 0.1) {
-        let pt: Point = calcQuadraticAtT(pts, t);
+        const pt: Point = calcQuadraticAtT(pts, t);
         lines.push(new Line(curr!, pt));
         curr = pt;
       }
     }
     if (cmd[0] == PATH_COMMAND.BEZIER_CURVE) {
-      let pts: Point[] = [curr, cmd[1], cmd[2], cmd[3]];
+      const pts: Point[] = [curr, cmd[1], cmd[2], cmd[3]];
       for (let t = 0; t < 1; t += 0.1) {
-        let pt = calcBezierAtT(pts, t);
+        const pt = calcBezierAtT(pts, t);
         lines.push(new Line(curr!, pt));
         curr = pt;
       }
--- scripts/src/lineamp.ts
+++ scripts/src/lineamp.ts
@@ -1,21 +1,10 @@
-/*
- _     _                                  
-| |   (_)_ __   ___  __ _ _ __ ___  _ __  
-| |   | | '_ \ / _ \/ _` | '_ ` _ \| '_ \ 
-| |___| | | | |  __/ (_| | | | | | | |_) |
-|_____|_|_| |_|\___|\__,_|_| |_| |_| .__/ 
-                                   |_|    
-Lineamp v0.3 2022/1/17 By Lampese
-*/
 import { BlockLocation } from "@minecraft/server";
+
 class Matrix {
-  /** 矩阵实现，实际是一个二维数组 */
   matrix: Array<Array<number>> = new Array<Array<number>>();
-  /**矩阵的行数 */
   row: number;
-  /**矩阵的列数 */
   column: number;
-  /**构造一个矩阵，r为行数，c为列数，val为矩阵的初值，默认为0。*/
+
   constructor(r: number, c: number, val = 0) {
     (this.row = r), (this.column = c);
     for (let i = 0; i < r; ++i) {
@@ -23,7 +12,7 @@ class Matrix {
       for (let j = 0; j < c; ++j) this.matrix[i][j] = val;
     }
   }
-  /**交换矩阵的a和b两行*/
+
   swap_row(a: number, b: number): void {
     if (a > this.row || b > this.row) throw new Error("The row is too big");
     let c: number;
@@ -33,7 +22,7 @@ class Matrix {
       this.matrix[b][i] = c;
     }
   }
-  /**交换矩阵中的a和b两列 */
+
   swap_column(a: number, b: number): void {
     if (a > this.column || b > this.column) throw new Error("The column is too big");
     let c: number;
@@ -43,24 +32,24 @@ class Matrix {
       this.matrix[i][b] = c;
     }
   }
-  /**对一整行元素进行映射*/
+
   map(r: number, f: (v: number) => number) {
     for (let i = 0; i < this.column; ++i) this.matrix[r][i] = f(this.matrix[r][i]);
   }
-  /**将矩阵第a行所有元素乘以一个数字后加到第b行(第a行元素不发生改变)*/
+
   add(a: number, b: number, k: number): void {
     if (a > this.row || b > this.row) throw new Error("The row is too big");
     for (let i = 0; i < this.column; ++i) this.matrix[b][i] += this.matrix[a][i] * k;
   }
-  /**对矩阵进行水平翻转 */
+
   fliphorizontal(): void {
     for (let i = 0; i < this.row / 2; ++i) this.swap_row(i, this.row - i + 1);
   }
-  /**对矩阵进行垂直翻转 */
+
   flipvertica(): void {
     for (let i = 0; i < this.column / 2; ++i) this.swap_column(i, this.column + 1);
   }
-  /**对矩阵进行主对角线翻转 */
+
   flipmdiagonal(): void {
     if (this.row != this.column) throw new Error("The row must be equal to the column");
     for (let i = 0; i < this.row; ++i)
@@ -70,7 +59,7 @@ class Matrix {
         this.matrix[j][i] = temp;
       }
   }
-  /**对矩阵进行次对角线翻转 */
+
   flipsdiagonal(): void {
     if (this.row != this.column) throw new Error("The row must be equal to the column");
     for (let i = 0; i < this.row; ++i)
@@ -80,15 +69,15 @@ class Matrix {
         this.matrix[this.row - j + 1][this.row - i + 1] = temp;
       }
   }
-  /**获取单行的 BlockLocation 对象 */
+
   getVector(row: number): BlockLocation {
     return new BlockLocation(this.matrix[row][0], this.matrix[row][1], this.matrix[row][2]);
   }
-  /**获取单列的 BlockLocation 对象*/
+
   getVectorCol(col: number): BlockLocation {
     return new BlockLocation(this.matrix[0][col], this.matrix[1][col], this.matrix[2][col]);
   }
-  /**把矩阵变为可输出的字符串，可用于debug */
+
   toString(): string {
     let result = "";
     for (let i = 0; i < this.row; ++i) {
@@ -99,13 +88,12 @@ class Matrix {
   }
 }
 namespace construct {
-  //**构造一个n*n的单位矩阵 */
   export function unit(n: number): Matrix {
     const result = new Matrix(n, n);
     for (let i = 0; i < n; ++i) result.matrix[i][i] = 1;
     return result;
   }
-  //**从二维数组构建矩阵 请保证下标从1开始 */
+
   export function fromArray(A: Array<Array<number>>): Matrix {
     const temp = new Matrix(A.length, A[0].length);
     temp.matrix = A;
@@ -113,21 +101,20 @@ namespace construct {
   }
 }
 namespace operation {
-  /**计算两个矩阵的和 */
   export function add(a: Matrix, b: Matrix): Matrix {
     if (a.row != b.row || a.column != b.column) throw new Error("Matrix size error");
     const result = a;
     for (let i = 0; i < a.row; ++i) for (let j = 0; j < a.column; ++j) result.matrix[i][j] += b.matrix[i][j];
     return result;
   }
-  /**计算两个矩阵的差 */
+
   export function sub(a: Matrix, b: Matrix): Matrix {
     if (a.row != b.row || a.column != b.column) throw new Error("Matrix size error");
     const result = a;
     for (let i = 0; i < a.row; ++i) for (let j = 0; j < a.column; ++j) result.matrix[i][j] -= b.matrix[i][j];
     return result;
   }
-  /**计算两个矩阵的乘积 */
+
   export function mul(a: Matrix, b: Matrix): Matrix {
     if (a.column != b.row) throw new Error("Matrix size error");
     const result = new Matrix(a.row, b.column);
@@ -136,7 +123,7 @@ namespace operation {
         for (let j = 0; j < b.column; ++j) result.matrix[i][j] += a.matrix[i][k] * b.matrix[k][j];
     return result;
   }
-  /**采用快速幂方法计算方形矩阵的幂a^p */
+
   export function pow(a: Matrix, p: number): Matrix {
     let result = construct.unit(a.row);
     while (p) {
@@ -146,7 +133,7 @@ namespace operation {
     }
     return result;
   }
-  /**比较两个矩阵是否相等 */
+
   export function equal(a: Matrix, b: Matrix): boolean {
     if (a.row != b.row || a.column != b.column) return false;
     for (let i = 0; i < a.row; ++i)
