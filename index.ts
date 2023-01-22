ceb56655e2f0381454aef7d8eb27bec635c54fa6
东灯 <43312495+Lampese@users.noreply.github.com>
2023-01-22 20:36:35 +0800

    code : fix import issue

---
+++ index.ts
@@ -0,0 +1,21 @@
+import { Vec3 } from "./src/vector";
+import * as Exp from "./src/expression";
+import * as Generator from "./src/generator";
+import * as Transform from "./src/transform";
+import * as LSystem from "./src/lsystem";
+import * as IFS from "./src/ifs";
+import { Turtle2D, Turtle3D } from "./src/turtle";
+import { DLA2D } from "./src/DLA2D.js";
+import { DLA3D } from "./src/DLA3D.js";
+export {
+  Vec3,
+  Exp,
+  Generator,
+  Transform,
+  LSystem,
+  IFS,
+  DLA2D,
+  DLA3D,
+  Turtle2D,
+  Turtle3D,
+};
--- release.js
+++
@@ -1,8 +0,0 @@
-const adm_zip = require('adm-zip');
-const fs=require('fs');
-const zip = new adm_zip();
-
-zip.addLocalFolder('./build/behavior_packs/gen/');
-zip.writeZip('VoxelGeometry.mcpack');
-
-fs.writeFileSync('version',JSON.parse(fs.readFileSync('./build/behavior_packs/gen/manifest.json'))["header"]["version"].join('.'));
