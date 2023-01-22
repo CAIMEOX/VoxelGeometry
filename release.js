6bc8a641e138bc37296fac2029ebbe2a4a546269
东灯 <43312495+Lampese@users.noreply.github.com>
2023-01-22 10:30:12 +0800

    action : file…s?

--- release.js
+++ release.js
@@ -1,4 +1,4 @@
 const adm_zip = require('adm-zip');
 const zip = new adm_zip();
-zip.addLocalFolder("./build/behavior_packs/gen/");
-zip.writeZip(`VoxelGeometry.mcpack`);
+zip.addLocalFolder('./build/behavior_packs/gen/');
+zip.writeZip('VoxelGeometry.mcpack');
