e32fd469137f1d8c1ac03a8ea734a1203a8d4543
东灯 <43312495+Lampese@users.noreply.github.com>
2023-01-23 21:12:34 +0800

    release : update to 1.1.0

--- src/vector.d.ts
+++
@@ -1,7 +0,0 @@
-declare class Vec3 {
-    x: number;
-    y: number;
-    z: number;
-    constructor(x: number, y: number, z: number);
-}
-export { Vec3 };
--- src/vector.js
+++
@@ -1,8 +0,0 @@
-class Vec3 {
-    constructor(x, y, z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-}
-export { Vec3 };
