8b3ed1961638903ee7616d8708a7a7ebf04ea5c3
CAIMEOX <CAIMEOX@users.noreply.github.com>
2023-07-20 18:52:16 +0800

    export Lineamp

--- index.ts
+++ index.ts
@@ -4,18 +4,20 @@ import * as Generator from "./src/generator.js";
 import * as Transform from "./src/transform.js";
 import * as LSystem from "./src/lsystem.js";
 import * as IFS from "./src/ifs.js";
+import * as Lineamp from "./src/lineamp.js";
 import { Turtle2D, Turtle3D } from "./src/turtle.js";
 import { DLA2D } from "./src/DLA2D.js";
 import { DLA3D } from "./src/DLA3D.js";
+const DLA = { DLA2D, DLA3D };
 export {
   Vec3,
   Exp,
   Generator,
   Transform,
+  Lineamp,
   LSystem,
   IFS,
-  DLA2D,
-  DLA3D,
+  DLA,
   Turtle2D,
   Turtle3D,
 };
